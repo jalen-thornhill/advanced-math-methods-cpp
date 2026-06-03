3b64cea07a69ca8bf4dbae68e52348ad9ff34323
Jalen Thornhill <by.jalen.by@outlook.com>
2026-06-02 20:22:42 -0400

    feat: add complex number properties

--- src/ComplexTool.cpp
+++ src/ComplexTool.cpp
@@ -1,4 +1,8 @@
 #include "ComplexTool.hpp"
+#include <cmath>
+#include <iostream>
+
+using namespace std;
 
 ComplexNumber::ComplexNumber(double r, double i) : real(r), imag(i) {}
 
@@ -8,6 +12,20 @@ double ComplexNumber::getReal() const {
 
 double ComplexNumber::getImag() const {
     return imag;
+}
+
+double ComplexNumber::modulus() const {
+    return sqrt(real * real + imag * imag);
+}
+
+double ComplexNumber::argumentRadians() const {
+    return atan2(imag, real);
+}
+
+void ComplexNumber::printPolarForm() const {
+    double r = modulus();
+    double theta = argumentRadians();
+    cout << "Polar form: " << r << " * (cos(" << theta << ") + i*sin(" << theta << "))" << endl;
 }
 
 ComplexNumber ComplexNumber::add(const ComplexNumber& a, const ComplexNumber& b) {
@@ -29,5 +47,9 @@ ComplexNumber ComplexNumber::divide(const ComplexNumber& a, const ComplexNumber&
     double realPart = (a.real * b.real + a.imag * b.imag) / denominator;
     double imagPart = (a.imag * b.real - a.real * b.imag) / denominator;
     return ComplexNumber(realPart, imagPart);
+}
+
+ComplexNumber ComplexNumber::conjugate() const {
+    return ComplexNumber(real, -imag);
 }
 
--- src/ComplexTool.hpp
+++ src/ComplexTool.hpp
@@ -10,9 +10,16 @@ public:
 
     double getReal() const;
     double getImag() const;
+    double modulus() const;
+    double argumentRadians() const;
+
+    void printPolarForm() const;
+
+
 
     ComplexNumber add(const ComplexNumber& a, const ComplexNumber& b);
     ComplexNumber subtract(const ComplexNumber& a, const ComplexNumber& b);
     ComplexNumber multiply(const ComplexNumber& a, const ComplexNumber& b);
     ComplexNumber divide(const ComplexNumber& a, const ComplexNumber& b);
+    ComplexNumber conjugate() const;
 };
