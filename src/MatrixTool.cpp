428fb5312eb0235b4c2a1f80a475f8109d8440c3
Jalen Thornhill <by.jalen.by@outlook.com>
2026-06-03 14:28:15 -0400

    feat: add 2x2 inverse calculation

--- src/MatrixTool.cpp
+++ src/MatrixTool.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void printMatrix(const Matrix& matrix) {
+void MatrixTool::printMatrix(const Matrix& matrix) {
     for (const auto& row : matrix) {
         for (const auto& value : row) {
             cout << setw(10) << value << " ";
@@ -13,15 +13,15 @@ void printMatrix(const Matrix& matrix) {
     }
 }
 
-bool sameDimensions(const Matrix& a, const Matrix& b) {
+bool MatrixTool::sameDimensions(const Matrix& a, const Matrix& b) {
     return (a.size() == b.size()) && (a.empty() || a[0].size() == b[0].size());
 }
 
-bool canMultiply(const Matrix& a, const Matrix& b) {
+bool MatrixTool::canMultiply(const Matrix& a, const Matrix& b) {
     return !a.empty() && !b.empty() && a[0].size() == b.size();
 }
 
-Matrix addMatrices(const Matrix& a, const Matrix& b) {
+Matrix MatrixTool::addMatrices(const Matrix& a, const Matrix& b) {
     if (!sameDimensions(a, b)) {
         throw invalid_argument("Matrices must have the same dimensions for addition.");
     }
@@ -34,7 +34,7 @@ Matrix addMatrices(const Matrix& a, const Matrix& b) {
     return result;
 }
 
-Matrix subtractMatrices(const Matrix& a, const Matrix& b) {
+Matrix MatrixTool::subtractMatrices(const Matrix& a, const Matrix& b) {
     if (!sameDimensions(a, b)) {
         throw invalid_argument("Matrices must have the same dimensions for subtraction.");
     }
@@ -48,7 +48,7 @@ Matrix subtractMatrices(const Matrix& a, const Matrix& b) {
 }
 
 
-Matrix multiplyMatrices(const Matrix& a, const Matrix& b) {
+Matrix MatrixTool::multiplyMatrices(const Matrix& a, const Matrix& b) {
     if (!canMultiply(a, b)) {
         throw invalid_argument("Number of columns in the first matrix must equal the number of rows in the second matrix for multiplication.");
     }
@@ -64,7 +64,7 @@ Matrix multiplyMatrices(const Matrix& a, const Matrix& b) {
 }
 
 
-Matrix transpose(const Matrix& matrix) {
+Matrix MatrixTool::transpose(const Matrix& matrix) {
     if (matrix.empty()) return {};
     Matrix result(matrix[0].size(), vector<double>(matrix.size()));
     for (size_t i = 0; i < matrix.size(); ++i) {
@@ -73,4 +73,56 @@ Matrix transpose(const Matrix& matrix) {
         }
     }
     return result;
+}
+
+double MatrixTool::determinant2x2(const Matrix& matrix) {
+    if (matrix.size() != 2 || matrix[0].size() != 2 || matrix[1].size() != 2) {
+        return 0;
+    }
+
+    return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
+}
+
+double MatrixTool::determinant3x3(const Matrix& matrix) {
+    if (matrix.size() != 3 ||
+        matrix[0].size() != 3 ||
+        matrix[1].size() != 3 ||
+        matrix[2].size() != 3) {
+        return 0;
+    }
+
+    return
+        matrix[0][0] * (
+            matrix[1][1] * matrix[2][2] -
+            matrix[1][2] * matrix[2][1]
+        )
+        -
+        matrix[0][1] * (
+            matrix[1][0] * matrix[2][2] -
+            matrix[1][2] * matrix[2][0]
+        )
+        +
+        matrix[0][2] * (
+            matrix[1][0] * matrix[2][1] -
+            matrix[1][1] * matrix[2][0]
+        );
+}
+
+Matrix MatrixTool::inverse2x2(const Matrix& matrix) {
+    if (matrix.size() != 2 || matrix[0].size() != 2 || matrix[1].size() != 2) {
+        return Matrix{};
+    }
+
+    double det = determinant2x2(matrix);
+
+    if (det == 0) {
+        return Matrix{};
+    }
+
+    Matrix inverse = {
+        { matrix[1][1] / det, -matrix[0][1] / det },
+        { -matrix[1][0] / det, matrix[0][0] / det }
+    };
+
+    return inverse;
 }
--- src/MatrixTool.hpp
+++ src/MatrixTool.hpp
@@ -4,8 +4,23 @@
 
 using Matrix = std::vector<std::vector<double>>;
 
+class MatrixTool {
+
+public:
 void printMatrix(const Matrix& matrix);
 
 bool sameDimensions(const Matrix& a, const Matrix& b);
 
 bool canMultiply(const Matrix& a, const Matrix& b);
+
+
+Matrix addMatrices(const Matrix& a, const Matrix& b);
+Matrix subtractMatrices(const Matrix& a, const Matrix& b);
+Matrix multiplyMatrices(const Matrix& a, const Matrix& b);
+Matrix transpose(const Matrix& matrix);
+Matrix inverse2x2(const Matrix& matrix);
+
+double determinant2x2(const Matrix& matrix);
+
+double determinant3x3(const Matrix& matrix);
+};
